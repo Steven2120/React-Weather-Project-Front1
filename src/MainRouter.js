546373a10176533bf1dbd67e0724c21330f50ce8
Steven Moyano <stevenmoyano1@gmail.com>
2021-07-27 12:05:14 -0500

    css error

--- src/MainRouter.js
+++ src/MainRouter.js
@@ -2,18 +2,24 @@ import React from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import Signup from "./components/Signup/SignUp";
 import Login from "./components/Login/Login";
+import Home from "./components/Home/Home";
+import Nav from "./components/Nav/Nav";
 
 const MainRouter = (props) => {
   return (
     <Router>
-      <Route exact path="/sign-up" component={Signup} />
-      <Route
-        exact
-        path="/login"
-        render={(routerProps) => (
-          <Login {...routerProps} handleUserLogin={props.handleUserLogin} />
-        )}
-      />
+      <Nav user={props.user} handleUserLogout={props.handleUserLogout} />
+      <>
+        <Route exact path="/sign-up" component={Signup} />
+        <Route
+          exact
+          path="/login"
+          render={(routerProps) => (
+            <Login {...routerProps} handleUserLogin={props.handleUserLogin} />
+          )}
+        />
+        <Route exact path="/" component={Home} />
+      </>
     </Router>
   );
 };
--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -1,11 +1,61 @@
 import React, { Component } from "react";
+import { Link, NavLink } from "react-router-dom";
+import "./Nav.css";
 
 export class Nav extends Component {
   render() {
+    console.log(this.props);
     return (
-      <div>
-        <div>Nav</div>
-      </div>
+      <nav className="Navbar">
+        <div className="h1-logo">
+          <h1>
+            <Link to="/">Weather App</Link>
+          </h1>
+        </div>
+
+        <div className="right-side-nav">
+          <ul>
+            <li>
+              {this.props.user ? (
+                <NavLink activeClassName="selected" to="/weather">
+                  Weather Search
+                </NavLink>
+              ) : (
+                ""
+              )}
+            </li>
+            <li>
+              {this.props.user ? (
+                <NavLink activeClassName="selected" to="/profile">
+                  Profile - {this.props.user.email}
+                </NavLink>
+              ) : (
+                <NavLink activeClassName="selected" to="/sign-up">
+                  Sign up
+                </NavLink>
+              )}
+            </li>
+            <li>
+              {this.props.user ? (
+                <NavLink
+                  activeStyle={{ borderBottom: "1px solid white" }}
+                  to="/"
+                  onClick={this.props.handleUserLogout}
+                >
+                  Logout
+                </NavLink>
+              ) : (
+                <NavLink
+                  activeStyle={{ borderBottom: "1px solid white" }}
+                  to="/login"
+                >
+                  Login
+                </NavLink>
+              )}
+            </li>
+          </ul>
+        </div>
+      </nav>
     );
   }
 }
