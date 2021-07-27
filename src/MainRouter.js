dc037ae4f8d8636f1bb506aef1d26afdf3755e35
Steven Moyano <stevenmoyano1@gmail.com>
2021-07-27 10:14:52 -0500

    completed Login portion of app

--- src/MainRouter.js
+++ src/MainRouter.js
@@ -1,11 +1,19 @@
 import React from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import Signup from "./components/Signup/SignUp";
+import Login from "./components/Login/Login";
 
-const MainRouter = () => {
+const MainRouter = (props) => {
   return (
     <Router>
       <Route exact path="/sign-up" component={Signup} />
+      <Route
+        exact
+        path="/login"
+        render={(routerProps) => (
+          <Login {...routerProps} handleUserLogin={props.handleUserLogin} />
+        )}
+      />
     </Router>
   );
 };
